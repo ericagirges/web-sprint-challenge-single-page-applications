0b3b81bf156fd35b82dff82e3302459e386fe0bc
Erica Girges <ericafiorenza@gmail.com>
2020-08-21 10:51:10 -0700

    completed MVP

--- src/App.js
+++ src/App.js
@@ -2,19 +2,46 @@ import React from "react";
 import OrderForm from "./components/OrderForm";
 import Home from "./components/Home";
 import { Link, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.div `
+  background-color: #E83703;
+  height: 150px;
+  display: flex;
+  justify-content: space-evenly;
+
+  h1{
+    font-family: "Courgette";
+    font-size: 4.0em;
+    color: white;
+    align-self: flex-end;
+  }
+
+`
+
+const Nav = styled.nav `
+    font-family: "Noto Sans KR";
+    font-size: 1.4em;
+    align-self: center;
+    color: white;
+    display: flex;
+    flex-direction: column;
+`
 
 const App = () => {
   return (
     <div>
+      <Header>
       <h1>Lambda Eats</h1>
-      <nav>
+      <Nav>
         <div className="nav-links">
           <Link to="/">Home</Link>
-          <Link to="/order-form">Place an Order</Link>
-        </div>
-      </nav>
+         <Link to="/order-form">Place an Order</Link>
+        </div>   
+      </Nav>
+      </Header>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
         </Route>
         <Route path="/order-form">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const Image = styled.img `
+ width: 80%;
+ height: auto;
+ margin-top: 40px;
+ border-radius: 10px;
+`
+const HeadHome = styled.h2`
+text-align: center;
+font-family: "Noto Sans KR";
+font-size: 2.0em;
+
+`
 
 const Home = props => {
 
     return (
-        <div></div>
+        <div>
+            <HeadHome>Order Now!</HeadHome>
+            <Image src="/pizza-home.jpg" alt="fresh cooked pizza"/>
+        </div>
     )
 }
 
